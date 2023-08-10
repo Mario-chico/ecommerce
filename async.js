d66b6e0f0f2c3ed9ef61ed0d10de84b94c9f1b3c
Mario Chico <chico.developer@hotmail.com>
2023-08-10 14:14:56 -0600

    using async await with fetch from node-fetch to consume an API

--- async.js
+++ async.js
@@ -1,3 +1,4 @@
+
 const fnAsync = () => {
   return new Promise((resolve, reject) => {
     (true) 
@@ -14,3 +15,27 @@ const anotherFn = async () => {
 console.log('Antes');
 anotherFn();
 console.log('Despues');
+import fetch from "node-fetch";
+const API = "https://api.escuelajs.co/api/v1"
+
+async function fetchData(urlApi){
+  const response = await fetch(urlApi);
+  const data = await response.json();
+  return data;
+}
+
+const anotherFunction = async (urlApi) => {
+  try{
+    const products = await fetchData(`${urlApi}/products`);
+    const product = await fetchData(`${urlApi}/products/${products[0].id}`);
+    const category=await fetchData(`${urlApi}/categories/${product.category.id}`);
+
+    //console.log(products);
+    console.log(product.title);
+    console.log(category.name);
+  }catch(err){
+    console.error(err)
+  }
+}
+
+anotherFunction(API)
